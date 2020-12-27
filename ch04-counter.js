580bbc04b4dd2ac116002c3d139715125679aaba
Slawomir Ernst <info@slawomirernst.de>
2020-12-27 14:02:57 +0100

    Add second neuralNetwork for training Data2 with other numbers & the train and run for it

--- ch04-counter.js
+++ ch04-counter.js
@@ -12,3 +12,16 @@ const stats = neuralNetwork.train(trainingData);
 console.log(stats);
 console.log(neuralNetwork.run([1, 2, 3]));
 console.log(neuralNetwork.run([5, 4, 3]));
+
+const trainingData2 = [
+  [5, 6, 7, 8, 9, 10],
+  [10, 9, 8, 7, 6, 5],
+];
+
+const neuralNetwork2 = new brain.recurrent.LSTMTimeStep();
+
+const stats2 = neuralNetwork2.train(trainingData2);
+
+console.log(stats2);
+console.log(neuralNetwork2.run([5, 6, 7]));
+console.log(neuralNetwork2.run([9, 8]));
