5b28dd4a6a97b60f715369349b621376ec27cdd0
Slawomir Ernst <info@slawomirernst.de>
2020-12-27 13:56:25 +0100

    Add brain, neuralNetwork with recurrence and LSTM and train

--- ch04-counter.js
+++ ch04-counter.js
@@ -1,4 +1,12 @@
+const brain = require("brain.js");
+
 const trainingData = [
   [1, 2, 3, 4, 5],
   [5, 4, 3, 2, 1],
 ];
+
+const neuralNetwork = new brain.recurrent.LSTMTimeStep();
+
+const stats = neuralNetwork.train(trainingData);
+
+console.log(stats);
