87e46765059017dea0d73383eb43c69059809533
Slawomir Ernst <info@slawomirernst.de>
2020-12-30 17:09:43 +0100

    Add training data

--- ch07-numberdetector.js
+++ ch07-numberdetector.js
@@ -100,4 +100,17 @@ function toArray(string) {
   return string.split("").map(toNumber);
 }
 
+const trainingData = [
+  { input: zero, output: { zero: 1 } },
+  { input: one, output: { one: 1 } },
+  { input: two, output: { two: 1 } },
+  { input: three, output: { three: 1 } },
+  { input: four, output: { four: 1 } },
+  { input: five, output: { five: 1 } },
+  { input: six, output: { six: 1 } },
+  { input: seven, output: { seven: 1 } },
+  { input: eight, output: { eight: 1 } },
+  { input: nine, output: { nine: 1 } },
+];
+
 const neuralNetwork = new brain.NeuralNetwork();
