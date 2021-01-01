0162cc9958888be5ff2e3f166192a360271bf104
Slawomir Ernst <info@slawomirernst.de>
2021-01-01 11:02:35 +0100

    Add brain and neuralNetwork

--- ch09-sentimentdetection.js
+++ ch09-sentimentdetection.js
@@ -1,6 +1,10 @@
+const brain = require("brain.js");
+
 const trainingData = [
   { input: "Happy new year!", output: "happy" },
   { input: "What a great 2020!", output: "sarcastic" },
   { input: "I'm super unhappy with the last one!", output: "sad" },
   { input: "Are we there yet?", output: "excited" },
 ];
+
+const neuralNetwork = new brain.recurrent.LSTM();
