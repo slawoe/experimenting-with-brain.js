e49d8ae445b8e760988c025c39d4f559cd5a57ab
Slawomir Ernst <info@slawomirernst.de>
2021-01-01 11:23:53 +0100

    Add some more running data and change training-plan ;)

--- ch09-sentimentdetection.js
+++ ch09-sentimentdetection.js
@@ -1,19 +1,21 @@
 const brain = require("brain.js");
 
 const trainingData = [
-  { input: "Happy new year!", output: "happy" },
-  { input: "What a great 2020!", output: "sarcastic" },
-  { input: "I'm super unhappy with the last one!", output: "sad" },
+  { input: "Have a great 2021!", output: "happy" },
+  { input: "It was a fantastic year!", output: "sarcastic" },
+  { input: "I'm super unhappy with 2020!", output: "sad" },
   { input: "Are we there yet?", output: "excited" },
 ];
 
 const neuralNetwork = new brain.recurrent.LSTM();
 
 neuralNetwork.train(trainingData, {
-  iterations: 300,
-  errorThresh: 0.0105,
-  log: (stats) => console.log(stats),
+  iterations: 100,
+  errorThresh: 0.011,
 });
 
-console.log(neuralNetwork.run("I am super unhappy!"));
-console.log("Test");
+console.log(neuralNetwork.run("Have a great 2021!"));
+console.log(neuralNetwork.run("It was a fantastic year!"));
+console.log(neuralNetwork.run("I'm super unhappy with 2020!"));
+console.log(neuralNetwork.run("Are we there yet?"));
+console.log(neuralNetwork.run("I love brain.js"));
