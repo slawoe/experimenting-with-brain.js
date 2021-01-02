0374e0fd2476059364a4488c3b6fa7f39fb8496c
Slawomir Ernst <info@slawomirernst.de>
2021-01-02 15:07:49 +0100

    Add brain and neuralNetwork

--- ch10-reinforcementlearning.js
+++ ch10-reinforcementlearning.js
@@ -1,6 +1,10 @@
+const brain = require("brain.js");
+
 const trainingData = [
   { input: [0, 0], output: [0] },
   { input: [0, 1], output: [1] },
-  { input: [1, 0], output: [1] },
-  { input: [1, 1], output: [0] },
+  //   { input: [1, 0], output: [1] },
+  //   { input: [1, 1], output: [0] },
 ];
+
+const neuralNetwork = new brain.NeuralNetwork({ hiddenLayers: [3] });
