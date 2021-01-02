cfa1172e7d713f2ccf874bbf4ea42c1f43d237e6
Slawomir Ernst <info@slawomirernst.de>
2021-01-02 15:22:16 +0100

    Add first reinforcement data, train again and run neuralNetwork

--- ch10-reinforcementlearning.js
+++ ch10-reinforcementlearning.js
@@ -3,14 +3,26 @@ const brain = require("brain.js");
 const trainingData = [
   { input: [0, 0], output: [0] },
   { input: [0, 1], output: [1] },
+  // JUST AS INFORMATION WHAT WILL BE ADDED TO ARRAY IN FUTURE FOR REINFORCEMENT
   //   { input: [1, 0], output: [1] },
   //   { input: [1, 1], output: [0] },
 ];
 
 const neuralNetwork = new brain.NeuralNetwork({ hiddenLayers: [3] });
 
-neuralNetwork.train(trainingData, { log: (stats) => console.log(stats) });
+neuralNetwork.train(trainingData);
 
+//BEFORE REINFORCEMENT
+console.log(neuralNetwork.run([0, 0]));
+console.log(neuralNetwork.run([0, 1]));
+console.log(neuralNetwork.run([1, 0]));
+console.log(neuralNetwork.run([1, 1]));
+
+// FIRST REINFORCEMENT
+trainingData.push({ input: [1, 0], output: [1] });
+neuralNetwork.train(trainingData);
+
+// AFTER FIRST REINFORCEMENT
 console.log(neuralNetwork.run([0, 0]));
 console.log(neuralNetwork.run([0, 1]));
 console.log(neuralNetwork.run([1, 0]));
