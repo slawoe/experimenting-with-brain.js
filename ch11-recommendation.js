a59205a78074a7f3961aeac6dd12a738ede6fdc7
Slawomir Ernst <info@slawomirernst.de>
2021-01-03 11:47:06 +0100

    Change values a second time, retrain and rerun

--- ch11-recommendation.js
+++ ch11-recommendation.js
@@ -17,10 +17,20 @@ console.log(neuralNetwork.run({ blue: 1 }));
 console.log(neuralNetwork.run({ brown: 1 }));
 
 // CHANGING TRAININGDATA
+console.log("adding 2nd values for brown");
 trainingData.push({ input: { brown: 1 }, output: [1] });
 neuralNetwork.train(trainingData);
 
 // AFTER CHANGE
-console.log("adding 2nd values for brown");
+console.log(neuralNetwork.run({ blue: 1 }));
+console.log(neuralNetwork.run({ brown: 1 }));
+
+// CHANGING 2ND TIME
+console.log("after deleting the first original value for brown");
+trainingData.splice(4);
+trainingData.push({ input: { brown: 1 }, output: [1] });
+neuralNetwork.train(trainingData);
+
+// AFTER 2ND CHANGE
 console.log(neuralNetwork.run({ blue: 1 }));
 console.log(neuralNetwork.run({ brown: 1 }));
