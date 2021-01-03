c3bb97fa1407444a029b377066bc2dc3d6821026
Slawomir Ernst <info@slawomirernst.de>
2021-01-03 11:22:21 +0100

    Add trainingData

---
+++ ch11-recommendation.js
@@ -0,0 +1,7 @@
+const trainingData = [
+  { input: { blue: 1 }, output: [1] },
+  { input: { red: 1 }, output: [1] },
+  { input: { black: 1 }, output: [0] },
+  { input: { green: 1 }, output: [0] },
+  { input: { brown: 1 }, output: [0] },
+];
