bcd0d04cf4cba4c4a96bf84460e7447496bcb0a6
Slawomir Ernst <info@slawomirernst.de>
2021-01-03 11:31:47 +0100

    Add more data for brown, retrain an rerun neuralNetwork

--- ch11-recommendation.js
+++ ch11-recommendation.js
@@ -12,5 +12,15 @@ const neuralNetwork = new brain.NeuralNetwork();
 
 neuralNetwork.train(trainingData);
 
+console.log("original");
+console.log(neuralNetwork.run({ blue: 1 }));
+console.log(neuralNetwork.run({ brown: 1 }));
+
+// CHANGING TRAININGDATA
+trainingData.push({ input: { brown: 1 }, output: [1] });
+neuralNetwork.train(trainingData);
+
+// AFTER CHANGE
+console.log("adding 2nd values for brown");
 console.log(neuralNetwork.run({ blue: 1 }));
 console.log(neuralNetwork.run({ brown: 1 }));
